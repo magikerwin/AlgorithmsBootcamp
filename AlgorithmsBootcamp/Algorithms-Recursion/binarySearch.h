24a813f05fb7ba28b4da3aee556ea3b803550c0b
kerwinhuang <qwerzxcv98@gmail.com>
2019-08-09 13:30:50 +0800

    Add binary search

---
+++ AlgorithmsBootcamp/Algorithms-Recursion/binarySearch.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+
+namespace binarySearch
+{
+	// [example.cpp]
+	// int test[5] = {1,2,3,10,50};
+	// std::cout << "Ans=" << binarySearch::binarySearch(test, 0, 5 - 1, 10) << std::endl;
+	
+	int binarySearch(int nums[], int begin, int end, int num)
+	{	if (begin > end)
+		{
+			std::cout << "[WARNING] Range error!" << std::endl;
+			return -1;
+		}
+
+		int middle = begin + (end - begin) / 2;
+
+		if (nums[middle] == num) return middle;
+
+		if (num < nums[middle])
+		{
+			return binarySearch(nums, begin, middle-1, num);
+		}
+		else {
+			return binarySearch(nums, middle+1, end, num);
+		}
+	}
+}
--- AlgorithmsBootcamp/main.cpp
+++ AlgorithmsBootcamp/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 //#include "Algorithms-Recursion/recursion.h"
 //#include "Algorithms-Recursion/factorial.h"
-#include "Algorithms-Recursion/fibonacci.h"
+//#include "Algorithms-Recursion/fibonacci.h"
+#include "Algorithms-Recursion/binarySearch.h"
 
 int main()
 {
-	std::cout << "Ans=" << fibonacci::tailFibonacci(2) << std::endl;
+	int test[5] = {1,2,3,10,50};
+	std::cout << "Ans=" << binarySearch::binarySearch(test,0,5-1,10) << std::endl;
 	std::system("pause");
-	return 0;
+	return 0;  
+
 }
